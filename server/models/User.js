448dd503e22421066af538892fff861f1a4a3189
Anojan Mylvaganam <m.anojan@live.fr>
2021-01-13 01:48:36 +0100

    started the project by creating server and client folders

---
+++ server/models/User.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+  firstName: String,
+  lastName: String,
+  profileImg: {
+    type: String,
+    default:
+      "https://vignette.wikia.nocookie.net/simpsons/images/1/14/Ralph_Wiggum.png/revision/latest/top-crop/width/360/height/360?cb=20100704163100",
+  },
+  nickname: String,
+  email: String,
+  password: String,
+});
+
+const User = mongoose.model("User", userSchema);
+
+module.exports = User;
